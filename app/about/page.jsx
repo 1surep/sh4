cd3e8059b350194e89497ad5dbe08b272fcb7544
Oghenevwede Louis Umukoro <168831499+unsurep@users.noreply.github.com>
2025-10-25 06:51:58 +0100

    about to start about us page

---
+++ app/about/page.jsx
@@ -0,0 +1,15 @@
+import About from "@/components/About/About";
+import React from "react";
+
+
+const AboutPage=()=>{
+    return (
+        <div>
+            <About/>
+        </div>
+    )
+
+
+
+};
+export default AboutPage;
---
+++ components/About/About.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const About=()=>{
+    return (
+        <div className="pt-32 min-h-screen px-[1rem] lg:px-[3rem]">
+            This is About Us Page
+        </div>
+    )
+
+
+
+};
+export default About;
