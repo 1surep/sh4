a8cdd0917d6a82e7018510287d718f9577955a55
Oghenevwede Louis Umukoro <168831499+unsurep@users.noreply.github.com>
2025-10-25 06:06:29 +0100

    contact us completed

---
+++ components/Contact/Contact.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Hero12 from "@/components/Hero/Hero12";
+import HeroThree from "../Hero/HeroThree";
+
+const Contact = () => {
+  return (
+    <div className="pt-32 min-h-screen ">
+      <Hero12 />
+      <HeroThree/>
+    </div>
+  );
+};
+export default Contact;
