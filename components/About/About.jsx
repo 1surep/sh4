9b68f24dace999197ffacff4f76c9249c7e6ef60
Oghenevwede Louis Umukoro <168831499+unsurep@users.noreply.github.com>
2025-10-31 00:12:41 +0100

    added image to about page under construction

--- components/About/About.jsx
+++ components/About/About.jsx
@@ -3,8 +3,8 @@ import React from "react";
 
 const About=()=>{
     return (
-      <div className="pt-32 min-h-screen px-[1rem] lg:px-[3rem]">
-        This is About Us Page
+      <div className="pt-32 h-screen px-[1rem] lg:px-[3rem] bg-no-repeat bg-cover" style={{ backgroundImage: "url('/uc.jpg')"}}>
+        
       </div>
     );
 
