42f86493b6f933a6a6acea4d09283258aa948847
Oghenevwede Louis Umukoro <168831499+unsurep@users.noreply.github.com>
2025-12-17 23:14:42 +0100

    try cursor

--- app/layout.js
+++ app/layout.js
@@ -36,6 +36,17 @@ function RootLayout({ children }) {
   return (
     <html lang="en" data-theme="light">
       <body className={`${outfit.variable} font-outfit antialiased`}>
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-W6W3NNK2');
+          `}
+        </Script>
+
         {/* Google Analytics */}
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-31P4QPST27"
@@ -49,6 +60,16 @@ function RootLayout({ children }) {
             gtag('config', 'G-31P4QPST27');
           `}
         </Script>
+
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-W6W3NNK2"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
 
         {loading && <Spinner />}
         <AuthProvider>
